fc0658d5247f4af6d4f8d3a2d7ec17e6d9816735
wildanjoe15-cpu <wildanjoe15@gmail.com>
2025-10-23 10:12:04 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Pilih tombol berdasarkan ID-nya
+cont tombol sapa = document getElementById( 'sapabutton');
+// 2. Tambahkan 'event listener' untuk aksi 'clik'
+tombolsapa.addEventListener('clik', function() (
+  // 3. Jalankan kode ini ketika tombol ini di-klik
+  alert('halo! Terima kasih sudah berkunjung!');
+});
